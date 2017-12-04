0a23441e79774fb17a01196778509935401b9d28
Sebastian Ściślak <scislak98@wp.pl>
2017-12-04 05:34:40 +0100

    Add simple foward move to player

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public GameObject prefab;
+    public Transform camTransform;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        Instantiate(prefab, camTransform.position, camTransform.rotation);
+        Debug.Log("created");
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public int speedFoward;
+	
+	void Start () {
+		
+	}
+	
+	void Update () {
+        transform.Translate(Vector2.right * speedFoward * Time.deltaTime);
+	}
+}
